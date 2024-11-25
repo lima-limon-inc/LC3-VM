d38453fb9277e46e62e0f18fd090ea26d805e930
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2024-11-25 15:39:47 -0300

    Add registers to vm struct

--- src/virtual_machine.rs
+++ src/virtual_machine.rs
@@ -3,12 +3,36 @@ const MEMORY_MAX: usize = 2_usize.pow(16);
 
 struct VM {
     memory: [u16; MEMORY_MAX],
+    r0: u16,
+    r1: u16,
+    r2: u16,
+    r3: u16,
+    r4: u16,
+    r5: u16,
+    r6: u16,
+    r7: u16,
+    rpc: u16,
+    rcond: u16,
+    rcount: u16,
 }
 
 impl VM {
     pub fn new() -> VM {
         let memory = [0; MEMORY_MAX];
-        VM { memory }
+        VM {
+            memory,
+            r0: 0,
+            r1: 0,
+            r2: 0,
+            r3: 0,
+            r4: 0,
+            r5: 0,
+            r6: 0,
+            r7: 0,
+            rpc: 0x300,
+            rcond: 0,
+            rcount: 0,
+        }
     }
 }
 
