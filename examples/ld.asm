064507b9e283d09204b5658d75b7ba70b47f63bd
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2024-11-29 16:53:05 -0300

    Added examples

--- examples/ld.asm
+++ examples/ld.asm
@@ -1,4 +1,7 @@
           .ORIG x3000                        ; this is the address in memory where the program will be loaded
-          LD R7, 42
-          HALT                               ; halt the program
+
+          ADD R5, R5, 9
+          ST R5, 5              ; ADDR = PC (3002) + 5 = 3007. Memory [ADDR] = R5 (9)
+          LD R6, 4              ; ADDR = PC (3003) + 4 = 3007. R6 = memory [ADDR]
+          HALT                  ; Halt
           .END
